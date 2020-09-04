8b9f671c88957bf62ed60d08a4f6a2ead59c5e8e
wanglinlin <w_llsh@sinosoft.com.cn>
2020-09-04 15:48:50 +0800

    test_5

--- TestCases/系统管理/test_user.py
+++ TestCases/系统管理/test_user.py
@@ -10,22 +10,22 @@ import TestDatas.login_test_datas as LTD
 P_log = Log(dir_config.processlog_dir)
 R_log = Log(dir_config.resultlog_dir)
 
-@pytest.mark.usefixture("init_web")
+@pytest.mark.usefixture("login_web")
 class Test_user:
     @pytest.mark.smoke
-    def test_newuser(self,init_web):
+    def test_newuser(self,login_web):
         P_log.info('*******开始执行新增用户测试用例******')
-        login(init_web).login_system(LTD.success_data['username'],LTD.success_data['pwd'])
-        First_Page(init_web).manager(2,'user')
-        User(init_web).add_user()
+        # login(login_web).login_system(LTD.success_data['username'],LTD.success_data['pwd'])
+        First_Page(login_web).manager(2,'user')
+        User(login_web).add_user()
         try:
             P_log.info("*******开始进行结果校验*********")
-            assert User(init_web).get_user(2) == News.newuser['name']
+            assert User(login_web).get_user(2) == News.newuser['name']
             R_log.info("******新增用户用例执行成功******")
         except Exception as e:
             R_log.info("新增用户用例执行失败")
             P_log.error("新增用户用例失败,失败原因:{0}".format(e))
-            login(init_web).save_picture('用例异常截图')
+            login(login_web).save_picture('用例异常截图')
             raise e
 
 if __name__ == '__main__':
