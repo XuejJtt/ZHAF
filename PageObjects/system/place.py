7b6e7e99bd3f7f6993702ae5f40f845f8a6cb577
wanglinlin <w_llsh@sinosoft.com.cn>
2020-11-03 10:19:47 +0800

    zzj update testcase

--- PageObjects/system/place.py
+++ PageObjects/system/place.py
@@ -34,15 +34,23 @@ class Area_managerment(BasePage):
     result_name='.ant-table-tbody [class="ant-table-row-cell-ellipsis ant-table-row-cell-break-word"]'#区域名称
     result_dis='.ant-table-tbody [class="ant-table-row-cell-ellipsis"]'#区域描述
     emptylist = 'ant-empty-description'  # 空列表，表示删除成功
+    area_errinfo='//div[@class="ant-form-explain"]'#区域（场所名称）错误提示语
 
 
-    def new_area(self):  # 新建区域
+    def new_area(self,areaname,areadisc):  # 新建区域
         self.click(self.add,By.CLASS_NAME)# 点击新增按钮
         self.clicks(self.wrapper,By.CSS_SELECTOR)# 点击新增区域按钮
-        self.input_text_m(self.area_text,place_date.newarea['areaname'],By.CSS_SELECTOR)# 输入区域名称
-        self.input_text_m(self.area_text, place_date.newarea['areadisc'],By.CSS_SELECTOR,10,'visible',False,1)  # 输入区域描述
+        self.input_text_m(self.area_text, areaname, By.CSS_SELECTOR)  # 输入区域名称
+        self.input_text_m(self.area_text, areadisc, By.CSS_SELECTOR, 10, 'visible', False,1)  # 输入区域描述
         self.click(self.Superior_list,By.CSS_SELECTOR)# 点击上级区域下拉框
         self.click(self.Superior_area,By.CLASS_NAME) # 选择上级区域
+        self.click(self.primary,By.CSS_SELECTOR)# 点击确定按钮
+
+    def area_Supernull(self,areaname,areadisc):  # 新建区域--上级区域为空
+        self.click(self.add,By.CLASS_NAME)# 点击新增按钮
+        self.clicks(self.wrapper,By.CSS_SELECTOR)# 点击新增区域按钮
+        self.input_text_m(self.area_text, areaname, By.CSS_SELECTOR)  # 输入区域名称
+        self.input_text_m(self.area_text, areadisc, By.CSS_SELECTOR, 10, 'visible', False,1)  # 输入区域描述
         self.click(self.primary,By.CSS_SELECTOR)# 点击确定按钮
 
     def modify_area(self):#修改区域名称和区域描述
@@ -58,7 +66,7 @@ class Area_managerment(BasePage):
         self.input_text(self.edit_areadis,place_date.editarea['areadisc'])#修改区域描述
         self.click(self.edit_confirm)#点击确定按钮
 
-    def deleteplace(self,n):
+    def deleteplace(self,n):#新增场所/区域
         em = self.find_element(self.name_search, By.CSS_SELECTOR)
         em.send_keys(Keys.CONTROL, 'a')
         self.input_text(self.name_search,Area_managerment.list1[n],By.CSS_SELECTOR)# 在搜索框中输入名称
@@ -66,20 +74,36 @@ class Area_managerment(BasePage):
         self.click(self.edit_confirm)#点击删除确认框中确定按钮
 
 
-
-
-    def new_place(self):
+    def new_place(self,placename,placedisc):#新增场所
         self.click(self.add, By.CLASS_NAME)  # 点击新增按钮
         self.clicks(self.wrapper,By.CSS_SELECTOR,10,'visible',1)# 点击新增场所按钮
-        self.input_text_m(self.area_text,place_date.newplace['placename'],By.CSS_SELECTOR)# 输入场所名称
+        self.input_text_m(self.area_text,placename,By.CSS_SELECTOR)# 输入场所名称
         self.click(self.place_type,By.CSS_SELECTOR) # 点击场所类型
         self.clicks(self.place_list,By.CLASS_NAME) # 选择下拉列表中的第一个
-        self.input_text_m(self.area_text, place_date.newplace['placedisc'], By.CSS_SELECTOR, 10, 'visible', False, 1)#输入场所描述
+        self.input_text_m(self.area_text, placedisc, By.CSS_SELECTOR, 10, 'visible', False, 1)#输入场所描述
         self.clicks(self.Superior_list,By.CSS_SELECTOR,10,'visible',1)#点击上级区域下拉框
         self.click(self.Superior_area,By.CLASS_NAME)#选择上级区域
         self.click(self.primary, By.CSS_SELECTOR)  # 点击确定按钮
 
-    def modify_place(self):
+    def place_typenull(self,placename,placedisc):#新增场所---场所类型为空
+        self.click(self.add, By.CLASS_NAME)  # 点击新增按钮
+        self.clicks(self.wrapper,By.CSS_SELECTOR,10,'visible',1)# 点击新增场所按钮
+        self.input_text_m(self.area_text,placename,By.CSS_SELECTOR)# 输入场所名称
+        self.input_text_m(self.area_text, placedisc, By.CSS_SELECTOR, 10, 'visible', False, 1)#输入场所描述
+        self.clicks(self.Superior_list,By.CSS_SELECTOR,10,'visible',1)#点击上级区域下拉框
+        self.click(self.Superior_area,By.CLASS_NAME)#选择上级区域
+        self.click(self.primary, By.CSS_SELECTOR)  # 点击确定按钮
+
+    def place_Supernull(self,placename,placedisc):#新增场所---上级区域为空
+        self.click(self.add, By.CLASS_NAME)  # 点击新增按钮
+        self.clicks(self.wrapper,By.CSS_SELECTOR,10,'visible',1)# 点击新增场所按钮
+        self.input_text_m(self.area_text,placename,By.CSS_SELECTOR)# 输入场所名称
+        self.click(self.place_type, By.CSS_SELECTOR)  # 点击场所类型
+        self.clicks(self.place_list, By.CLASS_NAME)  # 选择下拉列表中的第一个
+        self.input_text_m(self.area_text, placedisc, By.CSS_SELECTOR, 10, 'visible', False, 1)#输入场所描述
+        self.click(self.primary, By.CSS_SELECTOR)  # 点击确定按钮
+
+    def modify_place(self):#修改场所名称和场所描述
         em = self.find_element(self.name_search, By.CSS_SELECTOR)
         em.send_keys(Keys.CONTROL, 'a')
         self.input_text(self.name_search,place_date.newplace['placename'],By.CSS_SELECTOR)# 在搜索框中输入名称
@@ -91,17 +115,25 @@ class Area_managerment(BasePage):
         self.input_text_m(self.area_text, place_date.editplace['placedisc'], By.CSS_SELECTOR, 10, 'visible', False, 1)  # 修改场所描述
         self.click(self.primary, By.CSS_SELECTOR)  # 点击确定按钮
 
+
+
     def get_areainfo(self,n):
 
         em = self.find_element(self.name_search,By.CSS_SELECTOR)
         em.send_keys(Keys.CONTROL, 'a')
-        self.input_text(self.name_search,Area_managerment.list1[n],By.CSS_SELECTOR)#在搜索框中输入名称 list1 0:新增 区域名称 1：修改 区域名称  2：新增 场所名称  3：修改 场所名称
+
+        # 在搜索框中输入名称 list1 0:新增 区域名称 1：修改 区域名称  2：新增 场所名称  3：修改 场所名称
+        self.input_text(self.name_search,Area_managerment.list1[n],By.CSS_SELECTOR)
         name=self.get_text(self.result_name,By.CSS_SELECTOR)#区域名称
         dis=self.get_text(self.result_dis,By.CSS_SELECTOR)#区域描述
         list=[]
         list.append(name)
         list.append(dis)
         return list
+
+    def get_info_err(self):
+
+        return self.get_text(self.area_errinfo)#获取错误提示语
 
     def get_infonull(self,n):
         em = self.find_element(self.name_search, By.CSS_SELECTOR)
--- TestDatas/系统管理/newplace.py
+++ TestDatas/系统管理/newplace.py
@@ -11,7 +11,31 @@ i= random.randint(1,1000)
 newarea={'casename':'新增区域','areaname':'autotestarea'+str(i),'areadisc':'autotest区域描述'}
 #修改区域
 editarea={'casename':'修改区域','areaname':'editarea'+str(i),'areadisc':'edit区域描述'}
+
+#新增区域异常场景
+newarea_err=[{'casename':'新增区域--区域名称为空','areaname':'',
+              'areadisc':'区域名称为空','errinfo':'请输入区域名称'},
+
+             {'casename':'新增区域--区域名称超长','areaname':'区域名称超长'*10,
+              'areadisc':'区域名称大于50个字符','errinfo':'请输入1到50个字符'},
+
+             {'casename':'新增区域--上级区域为空','areaname':'上级区域为空',
+              'areadisc':'上级区域为空','errinfo':'请选择父节点'}]
+
 #新增场所
 newplace={'casename':'新增场所','placename':'autotestplace'+str(i),'placedisc':'仙鹤新天地广场'}
 #修改区域
 editplace={'casename':'修改场所','placename':'editplace'+str(i),'placedisc':'edit场所描述'}
+
+#新增场所异常场景
+newplace_err=[{'casename':'新增场所--场所名称为空','placename':'',
+               'placedisc':'仙鹤新天地广场','errinfo':'请输入场所名称'},
+
+              {'casename':'新增场所--场所名称超长','placename':'场所名称超长'*10,
+               'placedisc':'仙鹤新天地广场','errinfo':'请输入1到50个字符'},
+
+              {'casename':'新增场所--场所类型为空','placename':'场所类型为空',
+               'placedisc':'仙鹤新天地广场','errinfo':'请输选择场所类型'},
+
+              {'casename': '新增场所--上级区域为空', 'placename': '上级区域为空',
+               'placedisc': '仙鹤新天地广场', 'errinfo': '请选择上级区域'}]
